51d0b6b1368708e472ac98d8a6b5ef5631176b4c
AnastasiiaLarina <laranweb@gmail.com>
2024-02-04 21:23:49 +0500

    Create СheckSharingProject1

---
+++ СheckSharingProject1/СheckSharingProject1/_heckSharingProject1App.swift
@@ -0,0 +1,17 @@
+//
+//  _heckSharingProject1App.swift
+//  СheckSharingProject1
+//
+//  Created by Анастасия Ларина on 04.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct _heckSharingProject1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
