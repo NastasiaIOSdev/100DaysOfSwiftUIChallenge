de48d1284a7a7c74385bb6402a887b6f12af031a
AnastasiiaLarina <laranweb@gmail.com>
2024-02-25 00:19:52 +0500

    Task solution

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -38,33 +38,28 @@ class Expenses: ObservableObject {
 
 struct ContentView: View {
     @ObservedObject private var expenses = Expenses()
+    let types = ["Business", "Personal"]
     
     var body: some View {
         NavigationStack {
             List {
-                ForEach(expenses.items) { item in
-                    HStack{
-                        VStack(alignment: .leading) {
-                            Text(item.name)
-                                .font(.headline)
-                            Text(item.type)
-                        }
-                        Spacer()
-    
-                        if item.amount < 10 {
-                            Text(item.amount, format: .currency(code: "\(item.currency)"))
-                                .foregroundStyle(.blue)
-                        } else if item.amount < 100 {
-                            Text(item.amount, format: .currency(code: "\(item.currency)"))
-                                .foregroundStyle(.green)
-                        } else {
-                            Text(item.amount, format: .currency(code: "\(item.currency)"))
-                                .foregroundStyle(.red)
-                        }
+                Section(header: Text("Business")) {
+                    ForEach(expenses.items.filter { $0.type == "Business"}) { item in
+                        ExpenseRow(item: item)
                     }
-                    
+                    .onDelete(perform: { IndexSet in
+                        removeItems(at: IndexSet, type: "Business")
+                    })
                 }
-                .onDelete(perform: removeItems)
+                
+                Section(header: Text("Personal")) {
+                    ForEach(expenses.items.filter { $0.type == "Personal"}) { item in
+                        ExpenseRow(item: item)
+                    }
+                    .onDelete(perform: { IndexSet in
+                        removeItems(at: IndexSet, type: "Personal")
+                    })
+                }
             }
             .navigationTitle("iExpnse")
             .toolbar {
@@ -81,13 +76,45 @@ struct ContentView: View {
         }
     }
     
-    func removeItems(at offsets: IndexSet) {
-        expenses.items.remove(atOffsets: offsets)
+    func removeItems(at offsets: IndexSet, type: String) {
+        let itemsFilter = expenses.items.filter { $0.type == type }
+        let removeIndex = Array(offsets)
+        removeIndex.forEach { index in
+            if let index = expenses.items.firstIndex(where: { $0.id == itemsFilter[index].id }) {
+                expenses.items.remove(at: index)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ExpenseRow: View {
+    let item: ExpenceItem
+    
+    var body: some View {
+        HStack{
+            VStack(alignment: .leading) {
+                Text(item.name)
+                    .font(.headline)
+                Text(item.type)
+            }
+            Spacer()
+
+            if item.amount < 10 {
+                Text(item.amount, format: .currency(code: "\(item.currency)"))
+                    .foregroundStyle(.blue)
+            } else if item.amount < 100 {
+                Text(item.amount, format: .currency(code: "\(item.currency)"))
+                    .foregroundStyle(.green)
+            } else {
+                Text(item.amount, format: .currency(code: "\(item.currency)"))
+                    .foregroundStyle(.red)
+            }
+        }
     }
 }
